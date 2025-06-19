5aaa44d506cdae530eb2b3ace772a010d0790031
allysonedu <allysoneduu157@gmail.com>
2025-06-18 19:26:18 -0600

    'ajuste-tratamento-de-erro-pra-formato-json'

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -20,12 +21,20 @@ public class ExpensesController : ControllerBase
         }
         catch (ArgumentException ex)
         {
-            return BadRequest(ex.Message);
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = ex.Message
+            };
+            return BadRequest(errorResponse);
 
         }
         catch
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, "unknow error");
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = "unknow error"
+            };
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
     }
 }
