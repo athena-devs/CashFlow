eb0aa29b4c3a8795c84dd38dfb0516dcb3c4e951
allysonDrogal <allyson.batista@drogal.com.br>
2025-07-30 20:55:40 -0300

    'Criando-planilha-e-configurando-fontes'

--- src/CashFlow.Application/DependencyInjectionExtension.cs
+++ src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -1,10 +1,11 @@
 ﻿using CashFlow.Application.AutoMapper;
+using CashFlow.Application.UseCases.Expenses.Delete;
 using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
-using Microsoft.Extensions.DependencyInjection;
-using CashFlow.Application.UseCases.Expenses.Delete;
+using CashFlow.Application.UseCases.Expenses.Reports.Excel;
 using CashFlow.Application.UseCases.Expenses.Update;
+using Microsoft.Extensions.DependencyInjection;
 
 
 
@@ -30,6 +31,7 @@ public static class DependencyInjectionExtension
         services.AddScoped<IGetExpenseByIdUseCase, GetExpenseByIdUseCase>();
         services.AddScoped<IDeleteExpenseUseCase, DeleteExpenseUseCase>();
         services.AddScoped<IUpdateExpenseUseCase, UpdateExpenseUseCase>();
+        services.AddScoped<IGenerateExpensesReportExcelUseCase, GenerateExpensesReportExcelUseCase>();
 
 
     }
